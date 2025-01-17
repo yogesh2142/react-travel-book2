d1c039b996f9cd1647c46f352cda3d9385671809
Student yogesh2142 <Student yogeshnagar90381@gmail.com>
2025-01-17 11:57:02 +0530

    citylist css updated

--- src/components/Logo.jsx
+++ src/components/Logo.jsx
@@ -3,7 +3,7 @@ import style from "../styles/logo.module.css";
 function Logo() {
     return (
         <div className={style.logo}>
-            <img src="./logo.png" alt="logo" />
+            <img src="/logo.png" alt="logo" />
         </div>
     );
 }
